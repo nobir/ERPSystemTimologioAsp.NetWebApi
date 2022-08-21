38aee8ce6cc9b93b3c440a6b57bc8d2ec7fd90e2
Nobir <nobirfreelancer@gmail.com>
2022-08-22 00:15:00 +0600

    Initialize the project

---
+++ BLL/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    internal class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // create all the model map here
+            // Example: CreateMap<AddressModel, Address>();
+            //          CreateMap<Address, AddressModel>();
+
+
+        }
+    }
+}
---
+++ DAL/Interfaces/IRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IRepo<ClassType, PrimaryKeyType, MethodType>
+    {
+        List<ClassType> Gets();
+        ClassType Get(PrimaryKeyType id);
+        MethodType Create(ClassType obj);
+        MethodType Update(ClassType obj);
+        MethodType Delete(PrimaryKeyType id);
+    }
+}
