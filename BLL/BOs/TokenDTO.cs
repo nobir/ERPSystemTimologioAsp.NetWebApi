78772599e996acbc4c2452aac6e30237871e5bc5
Nobir <nobirfreelancer@gmail.com>
2022-08-23 07:20:16 +0600

    [Added] Business Object for Token and WrokingHour

---
+++ BLL/BOs/TokenDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BOs
+{
+    public class TokenDTO
+    {
+        public int Id { get; set; }
+        public string Token1 { get; set; }
+        public System.DateTime CreatedAt { get; set; }
+        public Nullable<System.DateTime> ExpiredAt { get; set; }
+        public int UserId { get; set; }
+
+        public virtual UserDTO User { get; set; }
+    }
+}
---
+++ BLL/BOs/WorkingHourDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.BOs
+{
+    public class WorkingHourDTO
+    {
+        public int Id { get; set; }
+        public System.DateTime Date { get; set; }
+        public System.DateTime EntryTime { get; set; }
+        public Nullable<System.DateTime> ExitTime { get; set; }
+        public int UserId { get; set; }
+
+        public virtual UserDTO User { get; set; }
+    }
+}
