b4f0fc91aff57636ec9b840e4ad238272871655b
Nobir <nobirfreelancer@gmail.com>
2022-08-23 07:11:00 +0600

    [Added] check authorization via Authorization in the headers

---
+++ ERPSystem/Controllers/ActionFilters/IsAuthorizedAttribute.cs
@@ -0,0 +1,39 @@
+﻿using BLL.BOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace ERPSystem.Controllers.ActionFilters
+{
+    public class IsAuthorizedAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var token = actionContext.Request.Headers.Authorization.ToString();
+            int user_id = ((dynamic)actionContext.ActionArguments.FirstOrDefault().Value).Id;
+
+            if(token.Length < 0 || token == null)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Invalid Token");
+
+                return;
+            }
+
+            var _token = TokenService.GetByTokenKeyUserIdExpiredNull(user_id, token);
+
+            if(_token == null)
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Invalid Token");
+
+                return;
+            }
+        }
+    }
+}
